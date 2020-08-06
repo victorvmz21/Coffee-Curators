344123ed744655f903fe9d285e12ac70e101f78f
Victor Monteiro <victor.monteiro21@icloud.com>
2020-08-06 10:53:33 -0600

    seeting up project

---
+++ Coffe Curators/Coffe Curators/Controller/View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffe Curators
+//
+//  Created by Victor Monteiro on 8/6/20.
+//  Copyright © 2020 Atomuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
