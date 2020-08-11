a639b169ae7fb6f7204e414e5636e417920bbee3
Victor Monteiro <victormonteiro@Victors-MacBook-Pro.local>
2020-08-11 14:15:07 -0600

    adding login and signup storyboard

---
+++ Coffe Curators/Coffe Curators/Controller/View Controller/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  Coffe Curators
+//
+//  Created by Victor Monteiro on 8/11/20.
+//  Copyright © 2020 Atomuz. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
