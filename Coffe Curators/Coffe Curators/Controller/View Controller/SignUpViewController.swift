d4f8d472c4a3b718ef28d203ad2686af8871a025
Victor Monteiro <victormonteiro@Victors-MacBook-Pro.local>
2020-08-11 14:34:00 -0600

    adding IBOutlets for testing

--- Coffe Curators/Coffe Curators/Controller/View Controller/SignUpViewController.swift
+++ Coffe Curators/Coffe Curators/Controller/View Controller/SignUpViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    //MARK: - IBOutlet
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passowordTextField: UITextField!
+    @IBOutlet weak var reenterPasswordTextField: UITextField!
+    
+    //MARK: Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - IBActions
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
     }
-    */
+    
+    //MARK: - Methods
 
 }
